7a3000edaad8c3111ab11a68804c825ec7088b2f
Sufyan Liaquat <sufyanliaquat58@gmail.com>
2026-05-30 23:51:57 +0500

    Updated requirements files for analytics, anomaly, auth, and certificate services; refactored tracing.js to use ES6 import syntax for Resource from OpenTelemetry.

--- services/grievanceService/src/utils/tracing.js
+++ services/grievanceService/src/utils/tracing.js
@@ -1,7 +1,7 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
-const { Resource } = require('@opentelemetry/resources');
+import { Resource }  from '@opentelemetry/resources';
 
 const sdk = new NodeSDK({
   resource: new Resource({
